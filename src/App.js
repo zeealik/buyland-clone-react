8274df6a6c35bfce1aa198664243b150c54a2e07
Zeeshan Ali Khan <zeealik7@gmail.com>
2021-02-12 22:39:20 +0500

    first push after tailwind integration

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+
+function App() {
+	return (
+		<div className="App">
+			<NavigationBar />
+		</div>
+	);
+}
+
+export default App;
